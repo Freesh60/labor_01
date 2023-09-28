953093f74058fec81fe46f1d9e1ae919b190a0e1
mpatrikik <m.patrik01@gmail.com>
2023-09-28 10:07:48 +0200

    4.szinkron

--- 04.py
+++ 04.py
@@ -1,23 +1,27 @@
 #számológép
 
-def adatkeres():
-    szam = input("Kérem az első számot: ")
-    while not szam.isnumeric():
-        print("Rossz érték!!!!")
+def adatkeres(tipus):
+    szam = ""
+    if tipus == "sz":
         szam = input("Kérem az első számot: ")
-    szam = int(szam)
+        while not szam.isnumeric():
+            print("Rossz érték!!!!")
+            szam = input("Kérem az első számot: ")
+        szam = int(szam)
+    elif tipus == "m":
+        pass
     return szam
 
 #program indítás
 print("Számológép")
-szam1 = adatkeres()
+szam1 = adatkeres("sz")
 
 muvelet = input("Milyen művelet legyen(+,-,*,/): ")
 while muvelet not in {"+","-","*","/"}:
     print("Nem jó a műveleti jel!!!!")
     muvelet = input("Milyen művelet legyen(+,-,*,/): ")
 
-szam2 = adatkeres()
+szam2 = adatkeres("sz")
 
 eredmeny = 0
 if muvelet == "+":

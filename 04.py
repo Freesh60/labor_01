150ecfe0143a866e1797d6bd5c6e83edfa669dad
mpatrikik <m.patrik01@gmail.com>
2023-09-28 09:56:45 +0200

    4.szinkron

--- 04.py
+++ 04.py
@@ -1,23 +1,25 @@
 #számológép
-print("Számológép")
 
-szam1 = input("Kérem az első számot: ")
-while not szam1.isnumeric():
-    print("Rossz érték!!!!")
-    szam1 = input("Kérem az első számot: ")
-szam1 = int(szam1)
+def adatkeres():
+    szam = input("Kérem az első számot: ")
+    while not szam.isnumeric():
+        print("Rossz érték!!!!")
+        szam = input("Kérem az első számot: ")
+    szam = int(szam)
+    return szam
+
+#program indítás
+print("Számológép")
+szam1 = adatkeres()
 
 muvelet = input("Milyen művelet legyen(+,-,*,/): ")
 while muvelet not in {"+","-","*","/"}:
     print("Nem jó a műveleti jel!!!!")
     muvelet = input("Milyen művelet legyen(+,-,*,/): ")
 
-szam2 = input("Kérem a második számot: ")
-while not szam2.isnumeric():
-    print("Rossz érték!!!!")
-    szam2 = input("Kérem a második számot: ")
-szam2 = int(szam2)
+szam2 = adatkeres()
 
+eredmeny = 0
 if muvelet == "+":
     eredmeny = szam1 + szam2
 elif muvelet == "-":
@@ -26,3 +28,10 @@ elif muvelet == "*":
     eredmeny = szam1 * szam2
 elif muvelet == "/":
     eredmeny = szam1 / szam2
+
+print(str (szam1).rjust(50))
+print(muvelet, end="")
+print(str (szam2).rjust(49))
+print("_".rjust(50, "_"))
+print(str(eredmeny).rjust(50))
+
